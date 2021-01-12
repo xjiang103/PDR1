a25d3a54059fcd4816a759727c361409598c9582
xjiang_RPi <xjiang103@gmail.com>
2021-01-12 10:38:54 -0600

    1st update

---
+++ PDR1/pdr1.py
@@ -0,0 +1,163 @@
+import serial
+import math
+from serial.tools import list_ports
+import serial
+import time
+
+
+class PDR1:
+    """Thorlabs PDR1 rotation stage class."""
+    def __init__(self,  ser_num):
+        """Set up and connect to device with serial number: ser_num"""
+        self.ser_num = ser_num
+        #self.chan_str='0'+str(chan_str)+'00'
+        self.ready = False
+        self.connect()
+        if not self.ready:
+            print('Unable to connect to device with serial number: {}'.format(ser_num))
+        else:
+            print('Connect successful to device with serial number: {}'.format(ser_num))
+    def connect(self):
+        """Searches all com ports for device with matching serial number
+        and opens a connection
+        """
+        ports = list_ports.comports()
+        for port in ports:
+            print(port)
+            # condition from from Xiayuo's old code, not sure if length check is necessary
+            # VID:PID=0403:faf0
+            # DWB: length check is not necessary...
+            if port[2][26:].startswith(self.ser_num):
+                self.ser = serial.Serial(baudrate=115200, timeout=0.1, port=port[0])
+                self.ready = True
+                break
+
+    def angle_to_DU(self, ang):
+        return int(ang*24576000/180)
+
+    def DU_to_angle(self, DU):
+        return (DU*180/24576000)
+
+    def dth(self, x, bytelen):
+        # print(x, '---', bytelen)
+        if x >= 0:
+            hstring = hex(x)
+            hstring = hstring[2:]
+            while(len(hstring) < 2*bytelen):
+                hstring = '0'+hstring
+            count = 0
+            new = list(hstring)
+            while count < bytelen*2:
+                tmp = new[count]
+                new[count] = new[count+1]
+                new[count+1] = tmp
+                count = count+2
+            hstring = ''.join(new)
+            hstring = hstring[::-1]
+            return hstring
+        elif x < 0:
+            y = abs(x)
+            bstring = bin(y)
+            bstring = bstring[2:]
+            while(len(bstring) < 2*bytelen*4):
+                bstring = '0'+bstring
+            # print(bstring)
+            count = 0
+            new = list(bstring)
+            while count < 2*bytelen*4:
+                if new[count] == '1':
+                    new[count] = '0'
+                else:
+                    new[count] = '1'
+                count = count+1
+
+            bstring = ''.join(new)
+            # print(bstring)
+            count = 2*bytelen*4-1
+            add = '1'
+            while count > -1:
+                if new[count] != add:
+                    add = '0'
+                    new[count] = '1'
+                else:
+                    new[count] = '0'
+                count = count-1
+            bstring = ''.join(new)
+            # print(bstring)
+            hstring = hex(int(bstring, 2))
+            hstring = hstring[2:]
+            while(len(hstring) < 2*bytelen):
+                hstring = '0'+hstring
+            count = 0
+            new = list(hstring)
+            while count < bytelen*2:
+                tmp = new[count]
+                new[count] = new[count+1]
+                new[count+1] = tmp
+                count = count+2
+            hstring = ''.join(new)
+            hstring = hstring[::-1]
+            lenhstring = len(hstring)
+            if lenhstring > 2*bytelen:
+                hstring = hstring[1:]
+            # print(hstring)
+            return hstring
+
+    def btd(self, x):
+        bytelen = len(x)
+        count = 0
+        dvalue = 0
+        while(count < bytelen):
+            dvalue = dvalue+x[count]*(math.pow(256, count))
+            count = count+1
+        bstring = bin(int(dvalue))
+        if len(bstring) < 2*bytelen*4+2:
+            return int(dvalue)
+        elif len(bstring) > 2*bytelen*4+2:
+            print('Error:Error in byte conversion')
+        else:
+            bstring = bin(int(dvalue-1))
+            bstring = bstring[2:]
+            count = 0
+            new = list(bstring)
+            while count < 2*bytelen*4:
+                if new[count] == '1':
+                    new[count] = '0'
+                else:
+                    new[count] = '1'
+                count = count+1
+            bstring = ''.join(new)
+            return (int(bstring, 2))*(-1)
+
+    def htb(self, x):
+        # print(x)
+        return bytearray.fromhex(x)
+
+    def rd(self, bytelen):
+        x = self.ser.readline()
+        while(len(x) < bytelen):
+            x = x+self.ser.readline()
+##        print(x)
+        return x
+
+    def write(self, x):
+        command = self.htb(x)
+        # print(command)
+        return self.ser.write(command)
+
+
+    def moveabs(self,chan_str,x):
+        abspos = self.dth(x, 4)
+        chan = '0'+str(chan_str)+'00'
+        header = 'd4080600d001'
+        hcmd = header+chan+abspos
+        print(hcmd)
+        self.write(hcmd)
+
+    def moverel(self,chan_str,x):
+        header0='c0080c00d0010500'
+        chan='0'+str(chan_str)+'00'
+        msg0='0000000000000000'
+        hcmd=header0+chan+msg0
+        self.write(hcmd)
+        self.moveabs(chan_str,x)
---
+++ test.py
@@ -0,0 +1,7 @@
+from PDR1.pdr1 import PDR1
+import sys
+import time
+m = PDR1('97100648')
+m.moverel(2,-100)
+print('test2')
+##m.home()
